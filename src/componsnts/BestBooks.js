f17a5f64021ef30bb2d3592ba4b9c14add4606ef
ibrahim-alhamshari <ibrahimalhamshari742@gmail.com>
2021-06-22 06:22:00 +0300

    some changes

--- src/componsnts/BestBooks.js
+++ src/componsnts/BestBooks.js
@@ -2,28 +2,27 @@ import React from 'react';
 import axios from 'axios';
 import { withAuth0 } from "@auth0/auth0-react";
 
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
 class Books extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            userName: this.props.auth0.user.name,
             userEmail: this.props.auth0.user.email,
-            userPicture: this.props.auth0.user.picture,
             serverUrl: process.env.REACT_APP_SERVER_URL,
             booksData: []
         }
     }
 
-
-    //this is a react function that will execute once the component rendered
     componentDidMount = () => {
-
-        axios.get(`http://localhost:8080/books?email=${this.state.userEmail}`).then(response => {
-            console.log(response);
+console.log(this.state.serverUrl);
+        axios.get(`${this.state.serverUrl}/books?email=${this.state.userEmail}`).then(response => {
+            // console.log(response);
             this.setState({
                 booksData: response.data[0].books,
             })
-            console.log(this.state.booksData.length);
+            // console.log(this.state.booksData.length);
         }).catch(
             error => {
                 alert(error.message);
@@ -34,12 +33,17 @@ class Books extends React.Component {
     render() {
         return (
             <>
-                <p> {this.state.userEmail} </p>
-
                 {this.state.booksData.length &&
                     this.state.booksData.map(value => {
                         return (
-                              <p>{value.name}</p>
+                            <Card style={{ width: '18rem' , margin:'7px' }}>
+                                <Card.Header>Books Data</Card.Header>
+                                <ListGroup variant="flush">
+                                    <ListGroup.Item>Name : {value.name}</ListGroup.Item>
+                                    <ListGroup.Item>Description : {value.description}</ListGroup.Item>
+                                    <ListGroup.Item>Status :{value.status}</ListGroup.Item>
+                                </ListGroup>
+                            </Card>
                         )
                     })
                 }
